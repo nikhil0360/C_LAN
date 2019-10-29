e181db606f9d133099bbaca38cbb4d93297c7611
avantikaaa <57094837+avantikaaa@users.noreply.github.com>
2019-10-29 14:14:37 +0530

    Update Q3.c
    Prints a triangle using stars

--- 2019/LAB1/3.c
+++ 2019/LAB1/3.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
-
 int main()
 {
-double c , f ;
-scanf( "%lf", &c ) ;
-f = (1.8*c) + 32 ;
-printf("%.2lf" , f ) ;
+	int n, i, j;
+	printf ("Number of rows: ");
+	scanf ("%d", &n);
+	for (i=1; i<=n; i++)
+	{
+		for (j=1; j<=n-i; j++)
+		{
+			printf(" ");
+		}
+		if (i==1)
+			printf ("*");
+		else if (1<i && i<n)
+		{
+			for (j=1; j<=2*i-1; j++)
+			{
+				if (j==1 || j== 2*i-1)
+				{
+					printf ("*");
+				}
+				else
+				{
+					printf (" ");
+				}
+			}
+		}
+		else if (i==n)
+		{
+			for (j=1; j<=2*n-1; j++)
+				printf ("*");
+		}
+		printf ("\n");
+	}
+	return 0;
 }
